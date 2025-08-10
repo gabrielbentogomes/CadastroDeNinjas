0a254e1c288285a908cec1184b44678cac27734f
Gabriel Bento <gabrielbgwork@gmail.com>
2025-08-09 23:12:11 -0300

    feat: service MissaoService

---
+++ src/main/java/dev/gabrielbento/CadastroDeNinjas/Missoes/Service/MissaoService.java
@@ -0,0 +1,20 @@
+package dev.gabrielbento.CadastroDeNinjas.Missoes.Service;
+
+import dev.gabrielbento.CadastroDeNinjas.Missoes.Model.MissaoModel;
+import dev.gabrielbento.CadastroDeNinjas.Missoes.Repository.MissoesRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MissaoService {
+    private MissoesRepository missoesRepository;
+
+    public MissaoService(MissoesRepository missoesRepository) {
+        this.missoesRepository = missoesRepository;
+    }
+
+    public List<MissaoModel> listarMissoes(){
+        return missoesRepository.findAll();
+    }
+}
