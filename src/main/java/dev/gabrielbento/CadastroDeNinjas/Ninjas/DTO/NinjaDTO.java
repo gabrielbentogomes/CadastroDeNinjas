b4a3eb5701a435eb0d52ec411e6beaa197ff4626
Gabriel Bento <gabrielbgwork@gmail.com>
2025-08-11 08:39:40 -0300

    feat: cria mapper e dto pra ninja

---
+++ src/main/java/dev/gabrielbento/CadastroDeNinjas/Ninjas/DTO/NinjaDTO.java
@@ -0,0 +1,4 @@
+package dev.gabrielbento.CadastroDeNinjas.Ninjas.DTO;
+
+public class NinjaDTO {
+}
---
+++ src/main/java/dev/gabrielbento/CadastroDeNinjas/Ninjas/Mapper/NinjaMapper.java
@@ -0,0 +1,4 @@
+package dev.gabrielbento.CadastroDeNinjas.Ninjas.Mapper;
+
+public class NinjaMapper {
+}
