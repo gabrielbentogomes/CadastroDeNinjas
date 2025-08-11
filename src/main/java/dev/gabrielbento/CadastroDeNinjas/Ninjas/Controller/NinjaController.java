80e4dc862724469cac9d2797bcce872387f5978c
Gabriel Bento <gabrielbgwork@gmail.com>
2025-08-11 13:10:21 -0300

    doc: adiciona Swagger a aplicação

--- src/main/java/dev/gabrielbento/CadastroDeNinjas/Ninjas/Controller/NinjaController.java
+++ src/main/java/dev/gabrielbento/CadastroDeNinjas/Ninjas/Controller/NinjaController.java
@@ -3,6 +3,7 @@ package dev.gabrielbento.CadastroDeNinjas.Ninjas.Controller;
 import dev.gabrielbento.CadastroDeNinjas.Ninjas.DTO.NinjaDTO;
 import dev.gabrielbento.CadastroDeNinjas.Ninjas.Model.NinjaModel;
 import dev.gabrielbento.CadastroDeNinjas.Ninjas.Service.NinjaService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +22,7 @@ public class NinjaController {
 
     // Adicionar ninja (CREATE)
     @PostMapping("/criar")
+    @Operation(summary = "Criar ninjas", description = "Essa rota permite salvar um ninja no banco de dados")
     public ResponseEntity<String> criarNinja(@RequestBody NinjaDTO ninjaDTO) {
         ninjaService.criarNinja(ninjaDTO);
         return ResponseEntity.ok("Ninja inserido com sucesso");
@@ -28,12 +30,14 @@ public class NinjaController {
 
     // Mostrar todos os ninjas (READ)
     @GetMapping("/listar")
+    @Operation(summary = "Listar ninjas", description = "Essa rota permite consultar todos os ninjas salvos no banco de dados")
     public List<NinjaDTO> listarNinjas() {
         return ninjaService.listarNinjas();
     }
 
     // Mostrar ninja  por id (READ)
     @GetMapping("/listar/{id}")
+    @Operation(summary = "Listar ninjas com ID", description = "Essa rota permite consultar um ninja com id no banco de dados")
     public ResponseEntity<?> listarNinjaPorId(@PathVariable Long id) {
          NinjaDTO ninja = ninjaService.listarPorId(id);
          if(ninja!=null)
@@ -44,6 +48,7 @@ public class NinjaController {
 
     // Alterar dados dos ninjas (UPDATE)
     @PutMapping("/alterar/{id}")
+    @Operation(summary = "Alterar ninjas", description = "Essa rota permite atualizar um ninja salvo no banco de dados atraves do id")
     public ResponseEntity<String> alterarNinjaPorId(@PathVariable Long id, @RequestBody NinjaDTO ninjaModel) {
         if(ninjaService.listarPorId(id)!=null) {
             ninjaService.atualizarNinja(id, ninjaModel);
@@ -56,6 +61,7 @@ public class NinjaController {
 
     // Deletar Ninja (DELETE)
     @DeleteMapping("/deletar/{id}")
+    @Operation(summary = "Deletar ninja", description = "Essa rota permite deletar ninja salvo no banco de dados pelo id")
     public ResponseEntity<String> deletarNinjaPorId(@PathVariable Long id) {
         if(ninjaService.listarPorId(id)!=null){
             ninjaService.deletarPorId(id);
